e8a1bd97742258674a17316a8ff3638dbfd0b09f
Chitwandeep Kaur Palne <chitwandeepkaurpalne@Chitwandeeps-MacBook-Air.local>
2024-02-22 17:50:47 +0530

    Login page and sign up page completed.

--- login/login.js
+++ login/login.js
@@ -1,6 +1,6 @@
-const users     = new Array(),
+const users     = JSON.parse(localStorage.getItem('users')),
       loginForm = document.getElementById('loginForm'),
-      usersMap = new Map()
+      usersMap  = new Map(JSON.parse(localStorage.getItem('userpins')))
 
 //Login Page Code
 loginForm.addEventListener('submit', (e) => {
@@ -8,11 +8,21 @@ loginForm.addEventListener('submit', (e) => {
     const data = loginForm.elements
     const username = data[0].value,
           userPIN  = data[1].value
-    if(usersMap.has(username) && usersMap.get(username) === userPIN) window.alert('Login successful')
+    if(usersMap.has(username) && usersMap.get(username) === userPIN){
+        window.alert('Login successful')
+        window.location.href = '../dashboard/dashboard.html'
+    }
     else if(usersMap.has(username) && usersMap.get(username) !== userPIN) window.alert('Password incorrect')
     else {
-        if(window.confirm(`User doesn't exist. Redirect to Sign up page?`)) window.location.replace("/signUp.html")
+        if(window.confirm(`User doesn't exist. Redirect to Sign up page?`)) window.location.replace("../signup/signUp.html")
     }
 })
 
+function showPassword() {
+    const pin = document.getElementById("pin")
+
+    if (pin.type === 'password') pin.type = "text"
+    else pin.type = "password"
+}
 //use the reduce method to calculate the balnace of the account
+//change window alerts to dialog boxes.
--- signup/signup.js
+++ signup/signup.js
@@ -1,22 +1,72 @@
 const signUpForm = document.getElementById('signupForm'),
-      users     = new Array(),
-      usersMap = new Map()
+      users      = JSON.parse(localStorage.getItem('users')) || new Array(),
+      usersMap   = new Map(JSON.parse(localStorage.getItem('userpins'))) || new Map(),
+      pinRegex   = /^\d{4}$/,
+      usernameRegex = /^[a-zA-Z0-9]{7,20}$/
 
 //Signup Page Code
 signUpForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const data = signUpForm.elements
+    data[3].value = Number(data[3].value)
+
+    if(!usernameRegex.test(data[0].value)) window.alert(`Username should be between 7 and 20 charaters!
+The username can have lower case and upper case characters and numbers.`)
+    else if(!pinRegex.test(data[3].value)) window.alert('PIN should be a 4 digit number')
+    else if(usersMap.has(String(data[0].value))) window.alert('Username already exists')
+    else createNewUserName(data)
+    //info box on focusing on input
+
+})
+
+const createNewUserName = function (data){
     users.push({
         username: data[0].value,
         firstName: data[1].value,
         lastName: data[2].value,
         pin: data[3].value,
     })
+
     usersMap.set(data[0].value, data[3].value)
+    localStorage.setItem('userpins',JSON.stringify([...usersMap]))
+    localStorage.setItem('users', JSON.stringify(users))
+
     //add age
     window.alert(`User Created Successfully.
 You will now be redirected to the Login Page`)
 
-//change this window alert to some dialog box or something
-    window.location.href='./index.html'
-})
+    window.location.href='../login/login.html'
+}
+
+function validatePassword() {
+ 
+    const pass         = document.getElementById('pin').value,
+          confirm_pass = document.getElementById('confirmPin').value,
+          wrong_pass_alert = document.getElementById('wrong_pass_alert'),
+          submitBtn =  document.getElementById('submitBtn')
+
+    if (pass !== confirm_pass) {
+        wrong_pass_alert.style.color = 'red'
+        wrong_pass_alert.innerHTML   = '☒ Use same password'
+        submitBtn.disabled = true;
+        submitBtn.style.opacity = (0.4);
+    } else {
+        wrong_pass_alert.style.color = 'green';
+        wrong_pass_alert.innerHTML =
+            '✅ Password Matched';
+        submitBtn.disabled = false;
+        submitBtn.style.opacity = (1);
+    }
+}
+
+function showPassword() {
+    const confirmPin = document.getElementById("confirmPin"),
+          pin        = document.getElementById("pin")
+    if (confirmPin.type === "password" && pin.type === 'password') {
+        confirmPin.type = "text"
+        pin.type = "text"
+    } else {
+      confirmPin.type = "password"
+      pin.type = "password"
+    }
+}
